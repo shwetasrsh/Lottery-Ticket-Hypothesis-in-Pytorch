f188d499b18a63e5f6287b9e80a4ab8faf863edb
shwetasrsh <shwetasrsh@gmail.com>
2020-04-12 15:15:15 +0530

    Update googlenet.py

--- archs/mnist/googlenet.py
+++ archs/mnist/googlenet.py
@@ -21,7 +21,7 @@ class Inception(nn.Module):
             nn.Conv2d(in_planes, n3x3red, kernel_size=1),
             nn.BatchNorm2d(n3x3red),
             nn.ReLU(True),
-            nn.Conv2d(n3x3red, n3x3, kernel_size=3, padding=1),
+            nn.Conv2d(n3x3red, n3x3, kernel_size=3, padding=2),
             nn.BatchNorm2d(n3x3),
             nn.ReLU(True),
         )
@@ -31,17 +31,17 @@ class Inception(nn.Module):
             nn.Conv2d(in_planes, n5x5red, kernel_size=1),
             nn.BatchNorm2d(n5x5red),
             nn.ReLU(True),
-            nn.Conv2d(n5x5red, n5x5, kernel_size=3, padding=1),
+            nn.Conv2d(n5x5red, n5x5, kernel_size=3, padding=2),
             nn.BatchNorm2d(n5x5),
             nn.ReLU(True),
-            nn.Conv2d(n5x5, n5x5, kernel_size=3, padding=1),
+            nn.Conv2d(n5x5, n5x5, kernel_size=3, padding=2),
             nn.BatchNorm2d(n5x5),
             nn.ReLU(True),
         )
 
         # 3x3 pool -> 1x1 conv branch
         self.b4 = nn.Sequential(
-            nn.MaxPool2d(3, stride=1, padding=1),
+            nn.MaxPool2d(3, stride=1, padding=2),
             nn.Conv2d(in_planes, pool_planes, kernel_size=1),
             nn.BatchNorm2d(pool_planes),
             nn.ReLU(True),
@@ -59,7 +59,7 @@ class GoogLeNet(nn.Module):
     def __init__(self):
         super(GoogLeNet, self).__init__()
         self.pre_layers = nn.Sequential(
-            nn.Conv2d(1, 192, kernel_size=3, padding=1),
+            nn.Conv2d(1, 192, kernel_size=3, padding=2),
             nn.BatchNorm2d(192),
             nn.ReLU(True),
         )
@@ -67,7 +67,7 @@ class GoogLeNet(nn.Module):
         self.a3 = Inception(192,  64,  96, 128, 16, 32, 32)
         self.b3 = Inception(256, 128, 128, 192, 32, 96, 64)
 
-        self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
+        self.maxpool = nn.MaxPool2d(3, stride=2, padding=2)
 
         self.a4 = Inception(480, 192,  96, 208, 16,  48,  64)
         self.b4 = Inception(512, 160, 112, 224, 24,  64,  64)
