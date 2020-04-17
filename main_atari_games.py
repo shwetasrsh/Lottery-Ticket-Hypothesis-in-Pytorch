3b333d881449f0f69b48ac3077770d4d7b271da2
shwetasrsh <shwetasrsh@gmail.com>
2020-04-18 00:08:28 +0530

    Create main_atari_games.py

---
+++ main_atari_games.py
@@ -0,0 +1,53 @@
+# Importing Libraries for main.py from LTH
+import argparse
+import copy
+import os
+import sys
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import matplotlib.pyplot as plt
+import os
+from tensorboardX import SummaryWriter
+import torchvision.utils as vutils
+import seaborn as sns
+import torch.nn.init as init
+import pickle
+
+# Custom Libraries for main.py for LTH
+import utils
+
+
+# Importing Libraries for dqn.py from minimalRL
+import gym
+import collections
+import random
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
