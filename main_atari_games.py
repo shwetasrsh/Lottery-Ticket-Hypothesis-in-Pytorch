f6418669fb518f8a80b1bf00f389061e07360943
shwetasrsh <shwetasrsh@gmail.com>
2020-04-22 00:38:10 +0530

    Update main_atari_games.py

--- main_atari_games.py
+++ main_atari_games.py
@@ -36,7 +36,338 @@ import torch.optim as optim
 
 
 
+# Tensorboard initialization  - main.py
+writer = SummaryWriter()
 
+# Plotting Style - main.py
+sns.set_style('darkgrid')
+
+
+# Main - main.py
+def main(args, ITE=0):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    reinit = True if args.prune_type=="reinit" else False
+    # Data Loader
+    env = gym.make('MsPacman-No-Frameskip-v0')
+    # Architecture
+    q = Qnet()
+    q_target = Qnet()
+    q_target.load_state_dict(q.state_dict())
+    memory = ReplayBuffer()
+    print_interval = 20
+    score = 0.0  
+    optimizer = optim.Adam(q.parameters(), lr=learning_rate)
+
+    for n_epi in range(10000):
+        epsilon = max(0.01, 0.08 - 0.01*(n_epi/200)) #Linear annealing from 8% to 1%
+        s = env.reset()
+        # reset function returns an initial observation
+        done = False
+
+        while not done:
+            a = q.sample_action(torch.from_numpy(s).float(), epsilon)      
+            s_prime, r, done, info = env.step(a)
+            # s_prime => an environment specific object representing your observation of the environment
+            # r => amount of reward achieved by the previous action. The scale varies between environments, but the goal is
+            # always to increase your total reward
+            # done => whether its time to reset the environment again. Most tasks are divided up into well defined episodes 
+            # and done being true indicates the episode has terminated.
+            # info => diagnostic information useful for debugging. 
+            done_mask = 0.0 if done else 1.0
+            memory.put((s,a,r/100.0,s_prime, done_mask))
+            s = s_prime
+
+            score += r
+            if done:
+                break
+            
+        if memory.size()>2000:
+            train(q, q_target, memory, optimizer)
+
+        if n_epi%print_interval==0 and n_epi!=0:
+            q_target.load_state_dict(q.state_dict())
+            print("n_episode :{}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
+                                                            n_epi, score/print_interval, memory.size(), epsilon*100))
+            score = 0.0
+    env.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Function for training - dqn.py
+def train(q, q_target, memory, optimizer):
+    for i in range(10):
+        s,a,r,s_prime,done_mask = memory.sample(batch_size)
+
+        q_out = q(s)
+        q_a = q_out.gather(1,a)
+        max_q_prime = q_target(s_prime).max(1)[0].unsqueeze(1)
+        target = r + gamma * max_q_prime * done_mask
+        loss = F.smooth_l1_loss(q_a, target)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        
+        
+# Prune by Percentile module
+def prune_by_percentile(percent, resample=False, reinit=False,**kwargs):
+        global step
+        global mask
+        global model
+
+        # Calculate percentile value
+        step = 0
+        for name, param in model.named_parameters():
+
+            # We do not prune bias term
+            if 'weight' in name:
+                tensor = param.data.cpu().numpy()
+                alive = tensor[np.nonzero(tensor)] # flattened array of nonzero values
+                percentile_value = np.percentile(abs(alive), percent)
+
+                # Convert Tensors to numpy and calculate
+                weight_dev = param.device
+                new_mask = np.where(abs(tensor) < percentile_value, 0, mask[step])
+                
+                # Apply new weight and mask
+                param.data = torch.from_numpy(tensor * new_mask).to(weight_dev)
+                mask[step] = new_mask
+                step += 1
+        step = 0
+       
+
+      
+# Function to make an empty mask of the same size as the model
+def make_mask(model):
+    global step
+    global mask
+    step = 0
+    for name, param in model.named_parameters(): 
+        if 'weight' in name:
+            step = step + 1
+    mask = [None]* step 
+    step = 0
+    for name, param in model.named_parameters(): 
+        if 'weight' in name:
+            tensor = param.data.cpu().numpy()
+            mask[step] = np.ones_like(tensor)
+            step = step + 1
+    step = 0
+
+    
+    
+def original_initialization(mask_temp, initial_state_dict):
+    global model
+    
+    step = 0
+    for name, param in model.named_parameters(): 
+        if "weight" in name: 
+            weight_dev = param.device
+            param.data = torch.from_numpy(mask_temp[step] * initial_state_dict[name].cpu().numpy()).to(weight_dev)
+            step = step + 1
+        if "bias" in name:
+            param.data = initial_state_dict[name]
+    step = 0
+
+    
+    
+# Function for Initialization
+def weight_init(m):
+    '''
+    Usage:
+        model = Model()
+        model.apply(weight_init)
+    '''
+    if isinstance(m, nn.Conv1d):
+        init.normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv2d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv3d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose1d):
+        init.normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose2d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose3d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.BatchNorm1d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.BatchNorm2d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.BatchNorm3d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.Linear):
+        init.xavier_normal_(m.weight.data)
+        init.normal_(m.bias.data)
+    elif isinstance(m, nn.LSTM):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.LSTMCell):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.GRU):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.GRUCell):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+                
+                
+                
+if __name__=="__main__":
+    
+    #from gooey import Gooey
+    #@Gooey      
+    
+    # Arguement Parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lr",default= 1.2e-3, type=float, help="Learning rate")
+    parser.add_argument("--batch_size", default=60, type=int)
+    parser.add_argument("--start_iter", default=0, type=int)
+    parser.add_argument("--end_iter", default=5, type=int)
+    parser.add_argument("--print_freq", default=1, type=int)
+    parser.add_argument("--valid_freq", default=1, type=int)
+    parser.add_argument("--resume", action="store_true")
+    parser.add_argument("--prune_type", default="lt", type=str, help="lt | reinit")
+    parser.add_argument("--gpu", default="0", type=str)
+    parser.add_argument("--dataset", default="mnist", type=str, help="mnist | cifar10 | fashionmnist | cifar100")
+    parser.add_argument("--arch_type", default="fc1", type=str, help="fc1 | lenet5 | alexnet | vgg16 | resnet18 | densenet121")
+    parser.add_argument("--prune_percent", default=10, type=int, help="Pruning percent")
+    parser.add_argument("--prune_iterations", default=1, type=int, help="Pruning iterations count")
+
+    
+    args = parser.parse_args()
+
+
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
+    os.environ["CUDA_VISIBLE_DEVICES"]=args.gpu
+    
+    
+    #FIXME resample
+    resample = False
+
+    # Looping Entire process
+    #for i in range(0, 5):
+    main(args, ITE=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# to plot ticket reward Vs fraction of weights pruned
+# so here we should have averaged episodic reward Vs prune percent(which will be the Compression rate)
+# ticket reward => averaged episodic reward over the last L game episodes
+# For each game we plot ticket reward curves for both winning and random tickets as the fraction of weight pruned increases
+
+# for plotting two or more lines on the same plot
+# x1 => Fraction of weights pruned - random ticket
+# y1 => Ticket reward - random ticket
+# x2 => Fraction of weights pruned - winning ticket
+# y2 => Ticket reward - winning ticket
+# plt.plot(x1, y1, label = "line 1")    
+# plt.plot(x2, y2, label = "line 2") 
+# plt.xlabel('x - axis')  
+# plt.ylabel('y - axis') 
+# plt.title('Ticket Reward Vs Fraction of weights pruned') 
+# plt.legend() 
+# plt.show() 
 
 
 
