64cfa2a680358c9a51ac816c294a318f67f7f97d
shwetasrsh <shwetasrsh@gmail.com>
2020-04-27 18:27:09 +0530

    Update resnext.py

--- archs/mnist/resnext.py
+++ archs/mnist/resnext.py
@@ -94,30 +94,46 @@ class BasicBlock_C(nn.Module):
         super(BasicBlock_C, self).__init__()
         inner_width = cardinality * bottleneck_width
         self.expansion = expansion
-        self.basic = nn.Sequential(OrderedDict(
-            [
-                ('conv1_0', nn.Conv2d(in_planes, inner_width, 1, stride=1, bias=False)),
-                ('bn1', nn.BatchNorm2d(inner_width)),
-                ('act0', nn.ReLU()),
-                ('conv3_0', nn.Conv2d(inner_width, inner_width, 3, stride=stride, padding=1, groups=cardinality, bias=False)),
-                ('bn2', nn.BatchNorm2d(inner_width)),
-                ('act1', nn.ReLU()),
-                ('conv1_1', nn.Conv2d(inner_width, inner_width * self.expansion, 1, stride=1, bias=False)),
-                ('bn3', nn.BatchNorm2d(inner_width * self.expansion))
-            ]
-        ))
+        #self.basic = nn.Sequential(OrderedDict(
+         #   [
+          #      ('conv1_0', nn.Conv2d(in_planes, inner_width, kernel_size=1, stride=1, bias=False)),
+           #     ('bn1', nn.BatchNorm2d(inner_width)),
+            #    ('act0', nn.ReLU()),
+             #   ('conv3_0', nn.Conv2d(inner_width, inner_width, 3, stride=stride, padding=1, groups=cardinality, bias=False)),
+              #  ('bn2', nn.BatchNorm2d(inner_width)),
+               # ('act1', nn.ReLU()),
+               # ('conv1_1', nn.Conv2d(inner_width, inner_width * self.expansion, 1, stride=1, bias=False)),
+               # ('bn3', nn.BatchNorm2d(inner_width * self.expansion))
+           #]
+      # ))
+        self.conv1 = nn.Conv2d(in_planes, inner_width, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(inner_width)
+        self.conv2 = nn.Conv2d(inner_width, inner_width, kernel_size=3, stride=stride, padding=1, groups=cardinality, bias=False)
+        self.bn2 = nn.BatchNorm2d(inner_width)
+        self.conv3 = nn.Conv2d(inner_width, self.expansion*inner_width, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*inner_width)
+
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != inner_width * self.expansion:
+        if stride != 1 or in_planes != inner_width*self.expansion:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, inner_width * self.expansion, 1, stride=stride, bias=False)
+                nn.Conv2d(in_planes, inner_width*self.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*inner_width)
             )
-        self.bn0 = nn.BatchNorm2d(self.expansion * inner_width)
+        #self.bn0 = nn.BatchNorm2d(self.expansion * inner_width)
 
     def forward(self, x):
-        out = self.basic(x)
+        #out = self.basic(x)
+        #out += self.shortcut(x)
+        #out = F.relu(self.bn0(out))
+        #return out
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
-        out = F.relu(self.bn0(out))
+        out = F.relu(out)
         return out
+
+
 
 class resnext(nn.Module):
     def __init__(self, num_blocks, cardinality, bottleneck_width, expansion=2, num_classes=10):
@@ -129,12 +145,12 @@ class resnext(nn.Module):
         
         self.conv0 = nn.Conv2d(3, self.in_planes, kernel_size=3, stride=1, padding=1)
         self.bn0 = nn.BatchNorm2d(self.in_planes)
-        self.pool0 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        #self.pool0 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1=self._make_layer(num_blocks[0],1)
         self.layer2=self._make_layer(num_blocks[1],2)
         self.layer3=self._make_layer(num_blocks[2],2)
-        self.layer4=self._make_layer(num_blocks[3],2)
-        self.linear = nn.Linear(self.cardinality * self.bottleneck_width, num_classes)
+        #self.layer4=self._make_layer(num_blocks[3],2)
+        self.linear = nn.Linear(self.cardinality*self.bottleneck_width*8, num_classes)
 
     def forward(self, x):
         out = F.relu(self.bn0(self.conv0(x)))
@@ -142,14 +158,14 @@ class resnext(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        #out = self.layer4(out)
+        out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
     def _make_layer(self, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
+        strides = [stride] + [1]*(num_blocks - 1)
         layers = []
         for stride in strides:
             layers.append(BasicBlock_C(self.in_planes, self.bottleneck_width, self.cardinality, stride, self.expansion))
@@ -159,35 +175,35 @@ class resnext(nn.Module):
 
 
 def resnext26_2x64d():
-    return resnext(num_blocks=[2, 2, 2, 2], cardinality=2, bottleneck_width=64)
+    return resnext(num_blocks=[3,3,3], cardinality=2, bottleneck_width=64)
 
 
 def resnext26_4x32d():
-    return resnext(num_blocks=[2, 2, 2, 2], cardinality=4, bottleneck_width=32)
+    return resnext(num_blocks=[3,3,3], cardinality=4, bottleneck_width=32)
 
 
 def resnext26_8x16d():
-    return resnext(num_blocks=[2, 2, 2, 2], cardinality=8, bottleneck_width=16)
+    return resnext(num_blocks=[3,3,3], cardinality=8, bottleneck_width=16)
 
 
 def resnext26_16x8d():
-    return resnext(num_blocks=[2, 2, 2, 2], cardinality=16, bottleneck_width=8)
+    return resnext(num_blocks=[3,3,3], cardinality=16, bottleneck_width=8)
 
 
 def resnext26_32x4d():
-    return resnext(num_blocks=[2, 2, 2, 2], cardinality=32, bottleneck_width=4)
+    return resnext(num_blocks=[3,3,3], cardinality=32, bottleneck_width=4)
 
 
 def resnext26_64x2d():
-    return resnext(num_blocks=[2, 2, 2, 2], cardinality=32, bottleneck_width=4)
+    return resnext(num_blocks=[3,3,3], cardinality=32, bottleneck_width=4)
 
 
 def resnext50_2x64d():
-    return resnext(num_blocks=[3, 4, 6, 3], cardinality=2, bottleneck_width=64)
+    return resnext(num_blocks=[3,3,3], cardinality=2, bottleneck_width=64)
 
 
 def resnext50_32x4d():
-    return resnext(num_blocks=[3, 4, 6, 3], cardinality=32, bottleneck_width=4)
+    return resnext(num_blocks=[3,3,3], cardinality=32, bottleneck_width=4)
 
 # def test():
 #     net = resnext50_2x64d()
